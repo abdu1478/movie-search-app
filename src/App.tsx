69ed61d9ccb030ad3fcc62be39108679fde8a0ef
abdu1478 <abduseid14789@gmail.com>
2025-04-14 16:27:35 +0300

    Added new features

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import { useMovieSearch, Movie } from "./hooks/useMovieSearch";
 import SearchBar from "./components/SearchBar.tsx";
 import MovieList from "./components/MovieList.tsx";
 import SelectedMovies from "./components/selectedMovies.tsx";
-import { inject } from "@vercel/analytics"
 
 function App() {
   const { movies, loading, error, fetchMovies } = useMovieSearch();
   const [selectedMovies, setSelectedMovies] = useState<Record<string, Movie>>({});
-  inject();
 
   const addMovie = (movie: Movie) => {
     setSelectedMovies((prev) => ({ ...prev, [movie.imdbID]: movie }));
